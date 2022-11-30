5980a42ad3c3ede7a0681ee11b37e23531743e62
Pierre Abbat <phma@bezitopo.org>
2022-11-30 09:34:33 +0000

    Initial commit, doesn't find Quadlods

---
+++ cubeorders.cpp
@@ -0,0 +1,15 @@
+/******************************************************/
+/*                                                    */
+/* cubeorders.cpp - hypercube corner orders           */
+/*                                                    */
+/******************************************************/
+
+#include <iostream>
+#include <cmath>
+#include <quadlods.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
