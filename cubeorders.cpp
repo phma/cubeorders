b7fa68269c2a2fe8999255520849b63c9732d387
Pierre Abbat <phma@bezitopo.org>
2022-12-02 09:02:33 +0000

    Count the number of times each order occurs

--- cubeorders.cpp
+++ cubeorders.cpp
@@ -8,17 +8,20 @@
 #include <cmath>
 #include <quadlods.h>
 #include "order.h"
+#define MINCOUNT 10
 using namespace std;
 
 int main(int argc, char *argv[])
 {
   int i,j;
   string order;
+  map<string,uint64_t> histo;
   init(5);
-  for (i=0;i<10;i++)
+  for (i=0;i<100000;i++)
   {
     order=gen();
-    cout<<order<<endl;
+    histo[order]++;
   }
+  cout<<histo.size()<<endl;
   return 0;
 }
