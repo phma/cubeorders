d5087506d0c842899144983b144b791778353800
Pierre Abbat <phma@bezitopo.org>
2022-12-04 00:42:29 +0000

    Keep generating orders until there appear to be no new ones

--- cubeorders.cpp
+++ cubeorders.cpp
@@ -11,16 +11,35 @@
 #define MINCOUNT 10
 using namespace std;
 
+bool ready(map<string,uint64_t> &histo)
+{
+  map<string,uint64_t>::iterator i;
+  bool ret=histo.size()>0;
+  for (i=histo.begin();ret && i!=histo.end();++i)
+    if (i->second<MINCOUNT)
+      ret=false;
+  return ret;
+}
+
 int main(int argc, char *argv[])
 {
   int i,j;
+  uint64_t count=MINCOUNT;
   string order;
   map<string,uint64_t> histo;
   init(5);
-  for (i=0;i<100000;i++)
+  while (count)
   {
     order=gen();
     histo[order]++;
+    if (histo[order]>MINCOUNT)
+    {
+      count--;
+      if (count==0 && !ready(histo))
+	count=histo.size();
+    }
+    else
+      count=histo.size();
   }
   cout<<histo.size()<<endl;
   return 0;
