f7eec9feda29b470f6ce7205224dfd54feb8caf1
Pierre Abbat <phma@bezitopo.org>
2022-12-11 07:52:00 +0000

    Add threads and random modules

---
+++ threads.cpp
@@ -0,0 +1,350 @@
+/******************************************************/
+/*                                                    */
+/* threads.cpp - multithreading                       */
+/*                                                    */
+/******************************************************/
+/* Copyright 2020-2022 Pierre Abbat.
+ * This file is part of Cubeorders.
+ *
+ * Cubeorders is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Cubeorders is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cubeorders. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <queue>
+#include <set>
+#include <cassert>
+#include <climits>
+#include <atomic>
+#include <cmath>
+#include <iostream>
+#include "threads.h"
+#include "random.h"
+
+using namespace std;
+namespace cr=std::chrono;
+
+#define CHUNKSIZE 137
+
+mutex actMutex;
+mutex startMutex;
+mutex anyThreadMutex;
+map<int,mutex> pointBufferMutex,cubeMutex;
+shared_mutex threadStatusMutex;
+mutex tileDoneMutex;
+mutex classTotalMutex;
+
+atomic<int> threadCommand;
+vector<thread> threads;
+vector<int> threadStatus; // Bit 8 indicates whether the thread is sleeping.
+vector<double> sleepTime;
+double stageTolerance;
+double minArea;
+queue<ThreadAction> actQueue,resQueue;
+int currentAction;
+map<thread::id,int> threadNums;
+
+cr::steady_clock clk;
+const char statusNames[][8]=
+{
+  "None","Run","Pause","Wait","Stop"
+};
+
+double busyFraction()
+{
+  int i,numBusy=0;
+  threadStatusMutex.lock_shared();
+  for (i=0;i<threadStatus.size();i++)
+    if ((threadStatus[i]&256)==0)
+      numBusy++;
+  threadStatusMutex.unlock_shared();
+  return (double)numBusy/i;
+}
+
+void startThreads(int n)
+{
+  int i,m;
+  threadCommand=TH_WAIT;
+  sleepTime.resize(n);
+  m=n*3;
+  threadNums[this_thread::get_id()]=-1;
+  for (i=0;i<n;i++)
+  {
+    threads.push_back(thread(OrderThread(),i));
+    sleepTime[i]=i+1;
+    this_thread::sleep_for(chrono::milliseconds(10));
+  }
+}
+
+void joinThreads()
+{
+  int i;
+  for (i=0;i<threads.size();i++)
+    threads[i].join();
+}
+
+ThreadAction dequeueAction()
+{
+  ThreadAction ret;
+  ret.opcode=0;
+  actMutex.lock();
+  if (actQueue.size())
+  {
+    ret=actQueue.front();
+    actQueue.pop();
+    if (ret.opcode)
+      currentAction=ret.opcode;
+  }
+  actMutex.unlock();
+  return ret;
+}
+
+void enqueueAction(ThreadAction a)
+{
+  actMutex.lock();
+  actQueue.push(a);
+  actMutex.unlock();
+}
+
+bool actionQueueEmpty()
+{
+  return actQueue.size()==0;
+}
+
+ThreadAction dequeueResult()
+{
+  ThreadAction ret;
+  ret.opcode=0;
+  actMutex.lock();
+  if (resQueue.size())
+  {
+    ret=resQueue.front();
+    resQueue.pop();
+  }
+  actMutex.unlock();
+  return ret;
+}
+
+void enqueueResult(ThreadAction a)
+{
+  actMutex.lock();
+  resQueue.push(a);
+  actMutex.unlock();
+}
+
+bool resultQueueEmpty()
+{
+  return resQueue.size()==0;
+}
+
+void sleep(int thread)
+{
+  sleepTime[thread]*=1.0625;
+  if (sleepTime[thread]>1e5)
+    sleepTime[thread]*=0.9375;
+  threadStatusMutex.lock();
+  threadStatus[thread]|=256;
+  threadStatusMutex.unlock();
+  this_thread::sleep_for(chrono::microseconds(lrint(sleepTime[thread])));
+  threadStatusMutex.lock();
+  threadStatus[thread]&=255;
+  threadStatusMutex.unlock();
+}
+
+void sleepDead(int thread)
+// Sleep to get out of deadlock.
+{
+  sleepTime[thread]*=1.0625;
+  if (sleepTime[thread]>1e5)
+    sleepTime[thread]*=0.9375;
+  threadStatusMutex.lock();
+  threadStatus[thread]|=256;
+  threadStatusMutex.unlock();
+  this_thread::sleep_for(chrono::microseconds(lrint(sleepTime[thread])));
+  threadStatusMutex.lock();
+  threadStatus[thread]&=255;
+  threadStatusMutex.unlock();
+}
+
+void unsleep(int thread)
+{
+  sleepTime[thread]*=0.9375;
+  if (sleepTime[thread]<1)
+    sleepTime[thread]*=1.125;
+  if (sleepTime[thread]<0 || std::isnan(sleepTime[thread]))
+    sleepTime[thread]=1;
+}
+
+double maxSleepTime()
+{
+  int i;
+  double max=0;
+  for (i=0;i<sleepTime.size();i++)
+    if (sleepTime[i]>max)
+      max=sleepTime[i];
+  return max;
+}
+
+void setThreadCommand(int newStatus)
+{
+  threadCommand=newStatus;
+  //cout<<statusNames[newStatus]<<endl;
+}
+
+int getThreadCommand()
+{
+  return threadCommand;
+}
+
+int getThreadStatus()
+/* Returns aaaaaaaaaabbbbbbbbbbcccccccccc where
+ * aaaaaaaaaa is the status all threads should be in,
+ * bbbbbbbbbb is 0 if all threads are in the same state, and
+ * cccccccccc is the state the threads are in.
+ * If all threads are in the commanded state, but some may be asleep and others awake,
+ * getThreadStatus()&0x3ffbfeff is a multiple of 1048577.
+ */
+{
+  int i,oneStatus,minStatus=-1,maxStatus=0;
+  threadStatusMutex.lock_shared();
+  for (i=0;i<threadStatus.size();i++)
+  {
+    oneStatus=threadStatus[i];
+    maxStatus|=oneStatus;
+    minStatus&=oneStatus;
+  }
+  threadStatusMutex.unlock_shared();
+  return (threadCommand<<20)|((minStatus^maxStatus)<<10)|(minStatus&0x3ff);
+}
+
+void setMinArea(double area)
+{
+  minArea=area;
+}
+
+void waitForThreads(int newStatus)
+// Waits until all threads are in the commanded status.
+{
+  int i,n;
+  threadCommand=newStatus;
+  do
+  {
+    threadStatusMutex.lock_shared();
+    for (i=n=0;i<threadStatus.size();i++)
+      if ((threadStatus[i]&255)!=threadCommand)
+	n++;
+    threadStatusMutex.unlock_shared();
+    this_thread::sleep_for(chrono::milliseconds(n));
+  } while (n);
+}
+
+void waitForQueueEmpty()
+// Waits until the action queue is empty and all threads have completed their actions.
+{
+  int i,n;
+  do
+  {
+    n=actQueue.size();
+    threadStatusMutex.lock_shared();
+    for (i=0;i<threadStatus.size();i++)
+      if (threadStatus[i]<256)
+	n++;
+    threadStatusMutex.unlock_shared();
+    //if (freeRam()<lowRam/4)
+      //cout<<"aoeu\n";
+    cout<<n<<"    \r";
+    cout.flush();
+    this_thread::sleep_for(chrono::milliseconds(30));
+  } while (n);
+}
+
+int thisThread()
+{
+  return threadNums[this_thread::get_id()];
+}
+
+int nThreads()
+// Returns number of worker threads, not counting main thread.
+{
+  return threadStatus.size();
+}
+
+void OrderThread::operator()(int thread)
+{
+  long long h=0,i=0,j=0,n,nPoints=0,nChunks;
+  long long blknum;
+  ThreadAction act;
+  startMutex.lock();
+  if (threadStatus.size()!=thread)
+  {
+    cout<<"Starting thread "<<threadStatus.size()<<", was passed "<<thread<<endl;
+    thread=threadStatus.size();
+  }
+  threadStatus.push_back(0);
+  threadNums[this_thread::get_id()]=thread;
+  startMutex.unlock();
+  while (threadCommand!=TH_STOP)
+  {
+    if (threadCommand==TH_READ)
+    { // The threads are reading the input files.
+      threadStatusMutex.lock();
+      threadStatus[thread]=TH_READ;
+      threadStatusMutex.unlock();
+      act=dequeueAction();
+      switch (act.opcode)
+      {
+      }
+    }
+    if (threadCommand==TH_PAUSE)
+    { // The job is ongoing, but has to pause to write out the files.
+      threadStatusMutex.lock();
+      threadStatus[thread]=TH_PAUSE;
+      threadStatusMutex.unlock();
+      act=dequeueAction();
+      switch (act.opcode)
+      {
+      }
+    }
+    if (threadCommand==TH_SCAN)
+    { // Scan the tiles to find the point density of the bottom.
+      threadStatusMutex.lock();
+      threadStatus[thread]=TH_SCAN;
+      threadStatusMutex.unlock();
+    }
+    if (threadCommand==TH_POSTSCAN)
+    { // After scanning, set the paraboloid size for forests and roofs.
+      threadStatusMutex.lock();
+      threadStatus[thread]=TH_POSTSCAN;
+      threadStatusMutex.unlock();
+    }
+    if (threadCommand==TH_SPLIT)
+    {
+      threadStatusMutex.lock();
+      threadStatus[thread]=TH_SPLIT;
+      threadStatusMutex.unlock();
+    }
+    if (threadCommand==TH_WAIT)
+    { // There is no job. The threads are waiting for a job.
+      threadStatusMutex.lock();
+      threadStatus[thread]=TH_WAIT;
+      threadStatusMutex.unlock();
+      if (thread)
+	act.opcode=0;
+      else
+	act=dequeueAction();
+    }
+  }
+  //octStore.flush(thread,threads.size());
+  threadStatusMutex.lock();
+  threadStatus[thread]=TH_STOP;
+  //cout<<"Thread "<<thread<<" processed "<<nPoints<<" points\n";
+  threadStatusMutex.unlock();
+}
---
+++ threads.h
@@ -0,0 +1,96 @@
+/******************************************************/
+/*                                                    */
+/* threads.h - multithreading                         */
+/*                                                    */
+/******************************************************/
+/* Copyright 2020-2022 Pierre Abbat.
+ * This file is part of Cubeorders.
+ *
+ * Cubeorders is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Cubeorders is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cubeorders. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef THREADS_H
+#define THREADS_H
+
+#ifdef __MINGW64__
+#define MINGW_STDTHREAD_REDUNDANCY_WARNING
+#include <../mingw-std-threads/mingw.thread.h>
+#include <../mingw-std-threads/mingw.mutex.h>
+#include <../mingw-std-threads/mingw.shared_mutex.h>
+#else
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+#endif
+#include <chrono>
+#include <vector>
+#include <array>
+#include <map>
+
+// These are used as both commands to the threads and status from the threads.
+#define TH_WAIT 1
+#define TH_READ 2
+#define TH_SCAN 3
+#define TH_POSTSCAN 4
+#define TH_SPLIT 5
+#define TH_PAUSE 6
+#define TH_STOP 7
+#define TH_ASLEEP 256
+
+// These are used to tell threads to do things, mostly file-related.
+#define ACT_READ 1
+#define ACT_COUNT 2
+#define ACT_WRITE 3
+#define ACT_LOAD 4
+
+#define RES_LOAD_PLY 1
+#define RES_LOAD_XYZ 2
+
+struct ThreadAction
+{
+  int opcode;
+  int param0;
+  double param1;
+  double param2;
+  std::string filename;
+  int flags;
+  int result;
+};
+
+extern int currentAction;
+extern std::chrono::steady_clock clk;
+
+double busyFraction();
+void startThreads(int n);
+void joinThreads();
+void enqueueAction(ThreadAction a);
+ThreadAction dequeueResult();
+bool actionQueueEmpty();
+bool resultQueueEmpty();
+void sleepDead(int thread);
+void setThreadCommand(int newStatus);
+int getThreadCommand();
+int getThreadStatus();
+void waitForThreads(int newStatus);
+void waitForQueueEmpty();
+int thisThread();
+int nThreads();
+
+class OrderThread
+{
+public:
+  void operator()(int thread);
+};
+
+#endif
